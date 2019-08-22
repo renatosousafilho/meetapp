8158003766b9a47fb15646a93b8c34eb779a93ed
Renato Filho <renatosousafilho@gmail.com>
2019-08-21 21:20:46 -0300

    configure sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'renato',
+    password: 'docker',
+    database: 'meetapp',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+
+}
